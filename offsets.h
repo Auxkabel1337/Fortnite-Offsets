af9fbb6fc84b12eddc6c0ecaf6f0094e3d8814b5
Auxkabel <115661100+Auxkabel1337@users.noreply.github.com>
2022-11-23 18:04:23 +0000

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,28 +1,28 @@
 #pragma once
 #include <Windows.h>
 
-namespace OFFSETS
+namespace offsets
 {
-    uintptr_t UWorld = 0xE0CF488;
-    uintptr_t AActor = 0x190;
-    uintptr_t Mesh = 0x310;
-    uintptr_t ActorCount = 0xA0;
-    uintptr_t CurrentActor = 0x8;
-    uintptr_t Revivefromdbnotime = 0x4180;
+    uintptr_t uWorld = 0xE0CF488;
+    uintptr_t aAcotr = 0x190;
+    uintptr_t mesh = 0x310;
+    uintptr_t actorCount = 0xA0;
+    uintptr_t currentActor = 0x8;
+    uintptr_t reviveFromdBNotime = 0x4180;
     uintptr_t TeamId = 0x1098;
-    uintptr_t LocalActorPos = 0x128;
-    uintptr_t ComponetToWorld = 0x240;
-    uintptr_t BoneArray = 0x5A0;
-    uintptr_t Velocity = 0x140;
-    uintptr_t PawnPrivate = 0x300;
-    uintptr_t PlayerArray = 0x2a0;
-    uintptr_t relativelocation = 0x2E8;
-    uintptr_t CapsuleComponent = 0x9E0;
-    uintptr_t Gameinstance = 0x1A8;
-    uintptr_t LocalPlayers = 0x38;
-    uintptr_t PlayerController = 0x30;
-    uintptr_t LocalPawn = 0x328;
-    uintptr_t PlayerState = 0x2A8;
-    uintptr_t RootComponet = 0x190;
-    uintptr_t PersistentLevel = 0x30;
+    uintptr_t localActorPos = 0x128;
+    uintptr_t componetToWorld = 0x240;
+    uintptr_t boneArray = 0x5A0;
+    uintptr_t velocity = 0x140;
+    uintptr_t pawnPrivate = 0x300;
+    uintptr_t playerArray = 0x2a0;
+    uintptr_t relativeLocation = 0x2E8;
+    uintptr_t capsuleComponent = 0x9E0;
+    uintptr_t gameInstance = 0x1A8;
+    uintptr_t localPlayers = 0x38;
+    uintptr_t playerController = 0x30;
+    uintptr_t localPawn = 0x328;
+    uintptr_t ülayerState = 0x2A8;
+    uintptr_t rootComponet = 0x190;
+    uintptr_t persistentLevel = 0x30;
 }
