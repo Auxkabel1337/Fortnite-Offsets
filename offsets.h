ae785c5efef6e2af226444724969a7ba788fcebd
Auxkabel <115661100+Auxkabel1337@users.noreply.github.com>
2022-12-11 11:21:11 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -5,18 +5,14 @@
 namespace offsets
 {
     // join discord.gg/pasterx for all offsets
-    uintptr_t UWorld = 0xE11FDC8;
+    uintptr_t UWorld = 0xe11fdc8;
     uintptr_t AActor = 0x98;
     uintptr_t Mesh = 0x310;
     uintptr_t ActorCount = 0xA0;
-    uintptr_t Revivefromdbnotime = 0x4250;
     uintptr_t TeamId = 0x10E8;
-    uintptr_t ComponetToWorld = 0x188;
     uintptr_t BoneArray = 0x5A0;
     uintptr_t PawnPrivate = 0x300;
     uintptr_t PlayerArray = 0x10C0;
-    uintptr_t relativelocation = 0x2E8;
-    uintptr_t CapsuleComponent = 0x9E0;
     uintptr_t Gameinstance = 0x3E8;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
