fddb7aa85bc8afe8d4258aacfca1036b70f6397a
Auxkabel <115661100+Auxkabel1337@users.noreply.github.com>
2022-12-13 18:18:56 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,23 +1,33 @@
 #pragma once
 #include <Windows.h>
 
-
 namespace offsets
 {
-    // join discord.gg/pasterx for all offsets
-    uintptr_t UWorld = 0xe11fdc8;
+    
+    uintptr_t UWorld = 0xe9ea3f8;
     uintptr_t AActor = 0x98;
     uintptr_t Mesh = 0x310;
     uintptr_t ActorCount = 0xA0;
-    uintptr_t TeamId = 0x10E8;
+    uintptr_t TeamId = 0x6a1;
     uintptr_t BoneArray = 0x5A0;
     uintptr_t PawnPrivate = 0x300;
-    uintptr_t PlayerArray = 0x10C0;
-    uintptr_t Gameinstance = 0x3E8;
+    uintptr_t PlayerArray = 0x2a0;
+    uintptr_t Gameinstance = 0x1b8;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
-    uintptr_t LocalPawn = 0x458;
+    uintptr_t LocalPawn = 0x328;
     uintptr_t PlayerState = 0x2a8;
     uintptr_t RootComponet = 0x190;
     uintptr_t PersistentLevel = 0x30;
+    uintptr_t Revivefromdbnotime = 0x4388;    
+}
+
+namespace W2S {
+    uintptr_t chain69 = 0xA8;
+    uintptr_t chain699 = 0x7E8;
+    uintptr_t chain = 0x70;
+    uintptr_t chain1 = 0x98;
+    uintptr_t chain2 = 0x140;   
+    uintptr_t vDelta = 0x10;
+    uintptr_t zoom = 0x580;
 }
